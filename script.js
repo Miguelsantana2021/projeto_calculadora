c72b77d4cdd864cbb927fe54d39d7165a2aa6cd9
Miguelsantana2021 <miguel.santana.santos@eaportal.org>
2024-03-31 17:54:04 -0300

    segundo js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 let conta = 0
 let pessoas = 0
+let porcentagem = 0
 
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
-
 function receberValorConta(evento) {
     conta = Number(evento.target.value)
 }
-
 const pessoasInput = document.querySelector("#pessoas")
 pessoasInput.addEventListener("input", receberQuantidadePessoas)
-
 function receberQuantidadePessoas(evento) {
-
-    const paragrafoErro = document.querySelector(".pessoa #erro")
+    const paragrafoErro = document.querySelector(".pessoas #erro")
     const divErro = document.querySelector(".pessoas .input-box")
-
     if(evento.target.value === "0") {
         paragrafoErro.style.display = "block"
         divErro.setAttribute("id", "erro-div")
@@ -24,4 +20,13 @@ function receberQuantidadePessoas(evento) {
         divErro.setAttribute("id", "")
         pessoas = Number(evento.target.value)
     }
+}
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", )
+})
+
+function receberPorcentagem(evento) {
+
 }
